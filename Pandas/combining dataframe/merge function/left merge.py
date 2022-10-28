c2047891b1c06827664754f7bfcc7de75d89e3d2
habib33-3 <habib.swi16@gamil.com>
2022-10-28 22:13:32 +0600

    combine dataframe

---
+++ Pandas/combining dataframe/merge function/left merge.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+registrations = pd.DataFrame({'reg_id': [1, 2, 3, 4], 'name': ['Andrew', 'Bobo', 'Claire', 'David']})
+logins = pd.DataFrame({'log_id': [1, 2, 3, 4], 'name': ['Xavier', 'Andrew', 'Yolanda', 'Bobo']})
+
+print(registrations)
+print(logins)
+
+x = pd.merge(left=registrations, right=logins, how="left", on="name")
+print(x)
