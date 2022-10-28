779cc602038e1f1ce09052e3dcfdc60f6e1ce2a3
habib33-3 <habib.swi16@gamil.com>
2022-10-28 21:34:46 +0600

    concatenation

---
+++ Pandas/combining dataframe/concatenation function.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data_one = {"A": ["A0", "A1", "C2", "A3"], "B": ["B0", "B1", "B2", "B3"]}
+data_two = {"C": ["C0", "C1", "C2", "C3"], "D": ["D0", "D1", "D2", "D3"]}
+
+one = pd.DataFrame(data_one)
+two = pd.DataFrame(data_two)
+
+print(one)
+print(two)
+
+print(pd.concat([one, two], axis=1))
+print(pd.concat([one, two], axis=0))  # not encouraged
+
+two.columns = one.columns
+df = pd.concat([one, two], axis=0)
+df.index = range(len(df))
+print(df)
